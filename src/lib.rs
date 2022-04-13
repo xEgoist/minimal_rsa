b5eafe89cdad276ad1c6848555416e7caf1be0c4
xEgoist <egoist@egoistic.dev>
2022-04-12 23:09:14 -0500

    Switched To UBig Previously, we couldn't calculate the modinv without crashing because of negative integers.

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,10 @@
 pub mod utils;
 
 use crate::utils::{pow_mod, IsPrime, Miller};
-use ibig::{ibig, IBig};
+use ibig::{UBig, ubig};
 use rand::prelude::*;
 use std::alloc::System;
+use std::sync::{Arc, Mutex};
 
 #[global_allocator]
 static A: System = System;
@@ -11,12 +12,12 @@ static A: System = System;
 #[allow(clippy::upper_case_acronyms)]
 #[derive(Default, Clone)]
 pub struct RSA {
-    pub phi: IBig,
-    pub p: IBig,
-    pub q: IBig,
-    pub pq: IBig,
-    pub e: IBig,
-    pub d: IBig,
+    pub phi: UBig,
+    pub p: UBig,
+    pub q: UBig,
+    pub pq: UBig,
+    pub e: UBig,
+    pub d: UBig,
 }
 
 impl RSA {
@@ -25,13 +26,13 @@ impl RSA {
         let mut rsa = RSA::default();
         rsa.p = generate_prime();
         rsa.q = rsa.p.clone();
-        while rsa.q == rsa.p && &rsa.q % &rsa.p == ibig!(0) {
+        while rsa.q == rsa.p && &rsa.q % &rsa.p == ubig!(0) {
             rsa.q = generate_prime();
         }
         rsa.pq = &rsa.p * &rsa.q.clone();
-        rsa.phi = (&rsa.p - ibig!(1)) * (&rsa.q - ibig!(1));
+        rsa.phi = (&rsa.p - ubig!(1)) * (&rsa.q - ubig!(1));
         // Fermat Prime (Used in OpenSSL)
-        rsa.e = ibig!(65537);
+        rsa.e = ubig!(65537);
         println!(
             "\np = {}\nq = {}\nphi={}\ne= {}\n",
             rsa.p, rsa.q, rsa.phi, rsa.e
@@ -41,27 +42,27 @@ impl RSA {
         rsa
     }
 
-    pub fn generate_prime_between_phi(&self) -> IBig {
+    pub fn generate_prime_between_phi(&self) -> UBig {
         let mut rng = thread_rng();
 
         loop {
-            let ret = rng.gen_range(ibig!(3)..self.phi.clone());
+            let ret = rng.gen_range(ubig!(3)..self.phi.clone());
             if ret.probably_prime(40) != IsPrime::Probably {
                 continue;
             }
-            if &self.phi % &ret == ibig!(0) {
+            if &self.phi % &ret == ubig!(0) {
                 continue;
             }
             return ret;
         }
     }
 
-    pub fn encrypt(&self, input: IBig) -> IBig {
+    pub fn encrypt(&self, input: UBig) -> UBig {
         pow_mod(input, self.e.clone(), &self.pq)
     }
-    pub fn decrypt(&self, input: IBig) -> IBig {
-        //let dp = &self.d % (&self.p - ibig!(1));
-        //let dq = &self.d % (&self.q - ibig!(1));
+    pub fn decrypt(&self, input: UBig) -> UBig {
+        //let dp = &self.d % (&self.p - ubig!(1));
+        //let dq = &self.d % (&self.q - ubig!(1));
 
         //crt(dq, dp, &self.p, &self.q, input)
         pow_mod(input, self.d.clone(), &self.pq)
@@ -76,64 +77,88 @@ pub enum RSAError {
 
 type Result<T> = std::result::Result<T, RSAError>;
 
-fn egcd(a: &IBig, b: &IBig) -> (IBig, IBig, IBig) {
-    if a == &ibig!(0) {
-        return (b.clone(), ibig!(0), ibig!(1));
+fn egcd(a: &UBig, b: &UBig) -> (UBig, UBig, UBig) {
+    if a == &ubig!(0) {
+        return (b.clone(), ubig!(0), ubig!(1));
     }
     let (g, y, x) = egcd(&(b % a), a);
-    (g, x - (b / a) * &y, y)
+    (g, x + (b / a) * &y, y)
 }
 
-pub fn modinv(a: &IBig, m: &IBig) -> Result<IBig> {
-    let (g, x, _y) = egcd(a, m);
-
-    if g != ibig!(1) {
+pub fn modinv(a: &UBig, m: &UBig) -> Result<UBig> {
+    let mut iter = 1;
+    let mut u1 = ubig!(1);
+    let mut u3 = a.clone();
+    let mut v1 = ubig!(0);
+    let mut v3 = m.clone();
+    while v3 != ubig!(0) {
+        let q = &u3 / &v3;
+        let t3 = &u3 % &v3;
+        let t1 = &u1 + q * &v1;
+        u1 = v1.clone();
+        v1 = t1;
+        u3 = v3.clone();
+        v3 = t3;
+        iter = -iter;
+    }
+    if u3 != ubig!(1) {
         return Err(RSAError::StandardEuclidean(
             "Something went wrong, you probably didn't supply a prime".to_owned(),
         ));
     }
-    let mut ret = x % m;
-    if ret < ibig!(0) {
-        ret += m;
+    if iter < 0 {
+        let inv = m - u1;
+        Ok(inv)
+    } else {
+        let inv = u1;
+        Ok(inv)
     }
-    Ok(ret)
 }
 
-pub fn crt(dq: IBig, dp: IBig, p: &IBig, q: &IBig, c: IBig) -> IBig {
-    let m1 = pow_mod(c.clone(), dp.clone(), &p);
-    let m2 = pow_mod(c, dq, &q);
-    let qinv = modinv(&q, &p).unwrap();
+pub fn crt(dq: UBig, dp: UBig, p: &UBig, q: &UBig, c: UBig) -> UBig {
+    let m1 = pow_mod(c.clone(), dp, p);
+    let m2 = pow_mod(c, dq, q);
+    let qinv = modinv(q, p).unwrap();
 
-    return ((((m1 - &m2) * qinv) % p) * q) + &m2;
+    ((((m1 - &m2) * qinv) % p) * q) + &m2
 }
 
-pub fn generate_prime() -> IBig {
-    let mut rng = thread_rng();
+pub fn generate_prime() -> UBig {
+    use rayon::prelude::*;
     eprint!("\rGenerating Prime.");
     loop {
-        let ret: IBig = rng.gen_range(ibig!(5000)..ibig!(2).pow(1024));
-        if ret.probably_prime(40) == IsPrime::Probably {
+        let ret = ubig!(0);
+        let mutexed = Arc::new(Mutex::new(ret));
+        (0..2048).into_par_iter().for_each(|x| {
+            let boo: bool = random();
+            let mut t = mutexed.lock().unwrap();
+            if boo {
+                t.set_bit(x);
+            }
+        });
+
+        if mutexed.lock().unwrap().probably_prime(40) == IsPrime::Probably {
             eprint!("\x1b[2K\r\n");
-            return ret;
+            return (*mutexed.lock().unwrap()).clone();
         }
         eprint!(".");
     }
 }
 
-pub fn numbify(input: &str) -> IBig {
-    let mut num = ibig!(0);
+pub fn numbify(input: &str) -> UBig {
+    let mut num = ubig!(0);
     for c in input.chars() {
         num = (num * 0x110000) + c as u8;
     }
     num
 }
 
-pub fn denumbify(input: IBig) -> String {
+pub fn denumbify(input: UBig) -> String {
     let mut v = vec![];
     let mut copy = input;
-    while copy != ibig!(0) {
-        v.push((&copy % (0x110000_u32)).to_f32() as u8 as char);
-        copy /= ibig!(0x110000);
+    while copy != ubig!(0) {
+        v.push((&copy % (0x110000_u32)) as u8 as char);
+        copy /= ubig!(0x110000);
     }
     v.reverse();
     String::from_iter(v)
@@ -141,16 +166,17 @@ pub fn denumbify(input: IBig) -> String {
 
 #[cfg(test)]
 mod tests {
+    use ibig::{UBig, ubig};
     use super::*;
 
     #[test]
     fn test_pow_mod_neg() {
-        assert_eq!(modinv(&ibig!(38), &ibig!(97)), Ok(ibig!(23)));
+        assert_eq!(modinv(&ubig!(38), &ubig!(97)), Ok(ubig!(23)));
     }
 
     #[test]
     fn it_works() {
-        assert_eq!(modinv(&ibig!(7), &ibig!(20)).unwrap(), ibig!(3));
+        assert_eq!(modinv(&ubig!(7), &ubig!(20)).unwrap(), ubig!(3));
     }
 
     #[test]
@@ -159,7 +185,7 @@ mod tests {
         assert_eq!(
             numbify(t),
             "212139510922239649191555332064962889369514977791303932739059812"
-                .parse::<IBig>()
+                .parse::<UBig>()
                 .unwrap()
         );
     }
@@ -169,7 +195,7 @@ mod tests {
         assert_eq!(
             denumbify(
                 "212139510922239649191555332064962889369514977791303932739059812"
-                    .parse::<IBig>()
+                    .parse::<UBig>()
                     .unwrap()
             ),
             "Hello World".to_owned()
--- src/utils.rs
+++ src/utils.rs
@@ -1,4 +1,4 @@
-use ibig::ibig;
+use ibig::ubig;
 use rand::{thread_rng, Rng};
 
 #[derive(PartialEq, Debug)]
@@ -7,31 +7,31 @@ pub enum IsPrime {
     NotPrime,
 }
 
-impl Miller for ibig::IBig {
+impl Miller for ibig::UBig {
     fn probably_prime(&self, rounds: u32) -> IsPrime {
-        if self == &ibig!(2) {
+        if self == &ubig!(2) {
             return IsPrime::Probably;
         }
-        if self % ibig!(2) == ibig!(0) {
+        if self % ubig!(2) == ubig!(0) {
             return IsPrime::NotPrime;
         }
 
-        let (mut r, mut s) = (0, self - ibig!(1));
-        while &s % ibig!(2) == ibig!(0) {
+        let (mut r, mut s) = (0, self - ubig!(1));
+        while &s % ubig!(2) == ubig!(0) {
             r += 1;
-            s /= ibig!(2);
+            s /= ubig!(2);
         }
         let mut ret = IsPrime::NotPrime;
         let mut rng = thread_rng();
         for _ in 0..=rounds {
-            let a = rng.gen_range(ibig!(2)..=self - ibig!(1));
+            let a = rng.gen_range(ubig!(2)..=self - ubig!(1));
             let mut x = pow_mod(a, s.clone(), self);
-            if x == ibig!(1) || x == (self - ibig!(1)) || &x % self == ibig!(0) {
+            if x == ubig!(1) || x == (self - ubig!(1)) || &x % self == ubig!(0) {
                 continue;
             }
             for _ in 0..r - 1 {
-                x = pow_mod(x, ibig!(2), self);
-                if x == (self - ibig!(1)) {
+                x = pow_mod(x, ubig!(2), self);
+                if x == (self - ubig!(1)) {
                     ret = IsPrime::Probably;
                     break;
                 }
@@ -48,12 +48,12 @@ pub trait Miller {
     fn probably_prime(&self, rounds: u32) -> IsPrime;
 }
 
-pub fn pow_mod(mut b: ibig::IBig, mut e: ibig::IBig, m: &ibig::IBig) -> ibig::IBig {
-    let mut num = ibig!(1);
+pub fn pow_mod(mut b: ibig::UBig, mut e: ibig::UBig, m: &ibig::UBig) -> ibig::UBig {
+    let mut num = ubig!(1);
     b = &b % m;
 
-    while e > ibig!(0) {
-        if &e & ibig!(1) != ibig!(0) {
+    while e > ubig!(0) {
+        if &e & ubig!(1) != ubig!(0) {
             num = (num * &b) % m;
         }
         e >>= 1;
@@ -65,53 +65,53 @@ pub fn pow_mod(mut b: ibig::IBig, mut e: ibig::IBig, m: &ibig::IBig) -> ibig::IB
 #[cfg(test)]
 mod tests {
     use super::*;
-    use ibig::{ibig, IBig};
+    use ibig::{UBig};
 
     #[test]
     fn it_works() {
         let result = 2 + 2;
         assert_eq!(result, 4);
-        let t = ibig!(3);
+        let t = ubig!(3);
         assert_eq!(IsPrime::Probably, t.probably_prime(40));
     }
 
     #[test]
     fn test_pow_mod_1() {
-        assert_eq!(ibig!(2), pow_mod(ibig!(5), ibig!(3), &ibig!(3)));
+        assert_eq!(ubig!(2), pow_mod(ubig!(5), ubig!(3), &ubig!(3)));
     }
 
     #[test]
     fn test_pow_mod_2() {
-        assert_eq!(ibig!(25), pow_mod(ibig!(5), ibig!(150), &ibig!(60)));
+        assert_eq!(ubig!(25), pow_mod(ubig!(5), ubig!(150), &ubig!(60)));
     }
 
     #[test]
     fn is_prime_25() {
-        let t = ibig!(25);
+        let t = ubig!(25);
         assert_eq!(IsPrime::NotPrime, t.probably_prime(40))
     }
 
     #[test]
     fn is_prime_31() {
-        let t = ibig!(31);
+        let t = ubig!(31);
         assert_eq!(IsPrime::Probably, t.probably_prime(40))
     }
 
     #[test]
     fn is_prime_101() {
-        let t = "191".parse::<IBig>().unwrap();
+        let t = "191".parse::<UBig>().unwrap();
         assert_eq!(IsPrime::Probably, t.probably_prime(40));
     }
 
     #[test]
     fn very_large_prime() {
-        let t = "1936738294519690982211090334402079885308248998113910860490043561062398610429619904537193501740559101".parse::<IBig>().unwrap();
+        let t = "1936738294519690982211090334402079885308248998113910860490043561062398610429619904537193501740559101".parse::<UBig>().unwrap();
         assert_eq!(IsPrime::Probably, t.probably_prime(40));
     }
 
     #[test]
     fn is_prime_zero() {
-        let t = ibig!(0);
+        let t = ubig!(0);
         assert_eq!(IsPrime::NotPrime, t.probably_prime(40));
     }
 }
